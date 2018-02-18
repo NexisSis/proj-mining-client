995ab34c5284d2ea527b9593694cc43978819ccb
Maxim Yakovenko <mxyakovenko9@gmail.com>
2018-02-18 18:36:15 +0300

    fix error in cabinet module

--- app/pages/Cabinet/index.js
+++ app/pages/Cabinet/index.js
@@ -1,1 +1,4 @@
 export {Panel} from "./Panel";
+export {Main} from "./Main";
+export {History} from "./History";
+export {Settings} from "./Settings";
--- app/pages/Main/Main.jsx
+++ app/pages/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom";
+import {Route,Switch} from "react-router-dom";
 import {Landing} from "app/pages";
 import {NoMatch} from "app/pages";
 import {Register} from "app/pages";
