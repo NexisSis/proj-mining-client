f322170ad242f9e1def81a7875b114d11535c094
Nexis <nexis.sis@gmail.com>
2018-02-19 23:17:30 +0300

    add bootstrap and bootstrap select to index file

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Container} from "app/main";
-import "bootstrap";
+import "bootstrap/";
+import "bootstrap-select/";
 
 ReactDOM.render(
     <Container />,
