cd9acf94d58823d08dae0ed017f54bb2449e86f1
Maxim Yakovenko <mxyakovenko9@gmail.com>
2018-02-25 16:06:22 +0300

    add opportunity page

---
+++ app/pages/Opportunities/Opportunities.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export class Opportunities extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Возможности
+            </div>
+        );
+    }
+}
+
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,5 +1,6 @@
 export {Main} from "./Main";
 export {Cabinet} from "./Cabinet";
+export {Oportunities} from "./Opportunities";
 export {Landing} from "./Landing";
 export {Register} from "./Register";
 export {NoMatch} from "./NoMatch";
