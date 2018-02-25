513571ad6f18d661178f84a5b020d86c9bfa96f2
Nexis <nexis.sis@gmail.com>
2018-02-25 11:21:53 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -153,7 +153,8 @@ module.exports = {
         }
     },
     devServer: {
-        port: 8080
+        port: 8080,
+        historyApiFallback: true
     },
     plugins: [
         new CleanWebpackPlugin("build"),
